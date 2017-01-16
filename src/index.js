7ea0c88444c56bdfb5d88a2b28c69bb6b1c0f24d
Jordi López <jordi.lopez@softonic.com>
2017-01-16 12:48:22 +0100

    Standard JS

--- src/index.js
+++ src/index.js
@@ -1,95 +1,95 @@
-import flatten  from 'lodash.flatten';
-import uniq  from 'lodash.uniq';
-import fs from 'fs';
+import flatten from 'lodash.flatten'
+import uniq from 'lodash.uniq'
+import fs from 'fs'
 
-const RE_CLASS = /class="([^"]*)"/gi;
-const DEFAULT_SELECTORS = ['html', 'body', 'h1', 'h2', 'h3'];
+const RE_CLASS = /class="([^"]*)"/gi
+const DEFAULT_SELECTORS = ['html', 'body', 'h1', 'h2', 'h3']
 
-let memory = {};
+let memory = {}
 
-function expressCriticalCSS({
+function expressCriticalCSS ({
   override = true,
   cssFile
 } = {}) {
-
-  const getClassSelectors = function({ content = '' }) {
-    let result = [];
-    let matches;
-    while (matches = RE_CLASS.exec(content)) {
-      result.push(matches[1].split(' '));
+  const getClassSelectors = function ({ content = '' }) {
+    let result = []
+    let matches
+    while (matches) {
+      result.push(matches[1].split(' '))
+      matches = RE_CLASS.exec(content)
     }
-    return uniq(flatten(result)).map(className => `.${className}`).concat(DEFAULT_SELECTORS);
-  };
+    return uniq(flatten(result)).map(className => `.${className}`).concat(DEFAULT_SELECTORS)
+  }
 
-  const getStylesheet = function() {
+  const getStylesheet = function () {
     return new Promise((resolve, reject) => {
       fs.readFile(cssFile, 'utf8', (err, file) => {
         if (err) {
-          return reject(err);
+          return reject(err)
         }
-        return resolve(file);
-      });
-    });
-  };
+        return resolve(file)
+      })
+    })
+  }
 
-  const extractCss = function({ stylesheet = '', selectors = []}) {
-    let styles = stylesheet.split('}');
-    let styleRules = [];
+  const extractCss = function ({ stylesheet = '', selectors = [] }) {
+    let styles = stylesheet.split('}')
+    let styleRules = []
     styles.forEach(style => {
-      style += '}';
-      style = style.replace(/\r\n|\n|\r|\s\s+/gm, ' ');
+      style += '}'
+      style = style.replace(/\r\n|\n|\r|\s\s+/gm, ' ')
       selectors.forEach(selector => {
         if (style.indexOf('@media') === -1 && style.indexOf(selector) !== -1) {
-          styleRules.push(style);
+          styleRules.push(style)
         }
-      });
-    });
-    return styleRules;
-  };
+      })
+    })
+    return styleRules
+  }
 
-  function crticalCSS(req, res, next) {
+  function crticalCSS (req, res, next) {
     const renderCallback = callback => {
       return (err, html) => {
         if (err) {
-          return next(err);
+          return next(err)
         }
-        const classSelectors = getClassSelectors({ content: html });
-        const memoryKey = classSelectors.join('');
+        const classSelectors = getClassSelectors({ content: html })
+        const memoryKey = classSelectors.join('')
 
         if (memory[memoryKey] || !cssFile) {
-          if(!cssFile) {
-            console.warning('express-inline-css: cssFile is required');
+          if (!cssFile) {
+            console.warning('express-inline-css: cssFile is required')
           }
-          res.send(html.replace(/(<head>.?)/g, `$1${memory[memoryKey] || ''}`));
+          res.send(html.replace(/(<head>.?)/g, `$1${memory[memoryKey] || ''}`))
         } else {
           getStylesheet().then(stylesheet => {
             const cssRules = extractCss({ stylesheet, selectors: classSelectors
-            }).join('');
-            const style = `<style>${cssRules}</style>`;
-            memory[memoryKey] = style;
-            res.send(html.replace(/(<head>.?)/g, `$1${style}`));
+            }).join('')
+            const style = `<style>${cssRules}</style>`
+            memory[memoryKey] = style
+            res.send(html.replace(/(<head>.?)/g, `$1${style}`))
           }).catch(err => {
-            return next(err);
-          });
+            return next(err)
+          })
         }
-      };
-    };
-
-    if (override === false) {
-      res.renderInlineCSS = function(view, renderOpts, callback) {
-        this.render(view, renderOpts, renderCallback(callback));
-      };
-    } else {
-      res.oldRender = res.render;
-      res.render = function(view, renderOpts, callback) {
-        this.oldRender(view, renderOpts, renderCallback(callback));
-      };
+      }
     }
 
-    return next();
+    if (override === false) {
+      res.renderInlineCSS = function (view, renderOpts, callback) {
+        this.render(view, renderOpts, renderCallback(callback))
+      }
+    } else {
+      res.oldRender = res.render
+      res.render = function (view, renderOpts, callback) {
+        this.oldRender(view, renderOpts, renderCallback(callback))
+      }
+    }
+
+    return next()
   }
 
-  return (crticalCSS);
+  return (crticalCSS)
 }
 
-export default expressCriticalCSS;
+export default expressCriticalCSS
