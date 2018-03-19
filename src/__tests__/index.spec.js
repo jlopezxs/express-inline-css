773a5426f75caf5df8945be6d41d8cc822c22628
Jordi López <jordi.lopez@softonic.com>
2018-03-19 10:39:05 +0100

    Update dependencies

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -1,36 +1,36 @@
-import inlineCSS from '../index';
+import inlineCSS from "../index";
 
-describe('inlineCSS', () => {
-  describe('when you want override render method', () => {
-    it('should be have render method', (done) => {
+describe("inlineCSS", () => {
+  describe("when you want override render method", () => {
+    it("should be have render method", done => {
       let req = {};
       let res = {};
 
-      const next = (err) => {
-        expect(res).toHaveProperty('render');
+      const next = () => {
+        expect(res).toHaveProperty("render");
         done();
       };
 
       inlineCSS({
         override: true,
-        cssFilePath: '../client/public/css/style.css'
+        cssFilePath: "../client/public/css/style.css"
       })(req, res, next);
     });
   });
 
-  describe('when you do NOT want override render method', () => {
-    it('should be have renderInlineCSS method', (done) => {
+  describe("when you do NOT want override render method", () => {
+    it("should be have renderInlineCSS method", done => {
       let req = {};
       let res = {};
 
-      const next = (err) => {
-        expect(res).toHaveProperty('renderInlineCSS');
+      const next = () => {
+        expect(res).toHaveProperty("renderInlineCSS");
         done();
       };
 
       inlineCSS({
         override: false,
-        cssFilePath: '../client/public/css/style.css'
+        cssFilePath: "../client/public/css/style.css"
       })(req, res, next);
     });
   });
